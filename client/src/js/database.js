92bb19630f74f58c005ee70c8aceb556cc967977
Jorge Gomez <jorge.electron@gmail.com>
2023-06-03 11:55:07 -0700

    adds methods to store and get content to DB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,33 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method to add contnet to the database
+export const putDb = async (content) => {  
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  console.log('Sending PUT command to the database');  
+  const jateDb = await openDB('jate', 1); // connects to DB
+  const tx = jateDb.transaction('jate', 'readwrite'); // starts a new transaction
+  const objStore = tx.objectStore('jate'); // opens object store
+  const req = objStore.add({ value: value }) // adds the content into DB
+  
+  // Confirming request succesful
+  const result = await request; 
+  console.log('Data saved to the database', result);
+}
+
+// Method to get all the content from the database
+export const getDb = async () => {
+
+  console.log('Sending GET command to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  
+  // Confirming request succesful
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+}
 
 initdb();
